28063ef2e8cc47d264a75bd08fd276ee34bf1753
abijiths-git <abijithsofficial@gmail.com>
2025-02-27 20:00:13 +0530

    EMC Tasks

---
+++ Error Makes Clever/JS/Ex 1.js
@@ -0,0 +1,71 @@
+/*
+var price=1000
+var product="Keyboard"
+var tax=200
+var total=price+tax
+console.log(product +" value is "+total )
+
+
+var fruitname= "Apple"
+var count= 10
+var price= 40
+var total=count*price;
+console.log("Price of "+count+" "+fruitname+" is "+total)
+
+
+function hello() {
+    console.log("Hello Everyone")
+}
+hello()
+
+
+var a=10
+var b=10
+function add(){
+    console.log(a+b)
+}
+add()
+
+
+factor="Kamal"
+fplayer="Dhoni"
+fmovie="Anbe Sivam"
+function fav(){
+    console.log("Favorite Actor: "+factor)
+    console.log("Favorite Player: "+fplayer)
+    console.log("Favorite Movie: "+fmovie)
+}
+fav()
+
+
+function area(l,b){
+    console.log("Area = "+l*b)
+}
+area(10,5)
+
+
+var a=10
+var b=10
+function add(){
+    return a+b
+}
+c=add()
+console.log(c)
+
+
+var hw = "spring"
+if (hw=="spring"){
+    console.log("SPRING")
+}
+if (hw=="summer"){
+    console.log("SUMMER")
+}
+if (hw=="rain"){
+    console.log("RAINY")
+}
+else{
+    console.log("Do your Homework")
+}
+
+*/
+
