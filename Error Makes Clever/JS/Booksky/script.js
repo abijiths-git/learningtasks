d93f09a463b03bfd79d635b9feeea12167907ac9
abijiths-git <abijithsofficial@gmail.com>
2025-03-09 19:35:46 +0530

    Booksky Task Completed

--- Error Makes Clever/JS/Booksky/script.js
+++ Error Makes Clever/JS/Booksky/script.js
@@ -1,13 +1,35 @@
-var addbtn = document.querySelector(".btn-dark");
-var bookbox = document.querySelector(".bookbox");
+var addbtn = document.querySelector(".addbtn");
+var bookbox = document.querySelector(".bookbox")
+var addpage = document.querySelector(".addbookpage")
+var canlbtn = document.querySelector(".canlbtn")
+var subbtn = document.querySelector(".subbtn")
 
-addbtn.addEventListener("click", () => {
+addbtn.addEventListener("click",()=>{
+    addpage.style.display = "flex";
+});
+canlbtn.addEventListener("click",()=>{
+    addpage.style.display = "none";
+});
+subbtn.addEventListener("click",()=>{
+    var bkhead = document.querySelector(".bkhead").value
+    var bkauthor = document.querySelector(".bkauth").value
+    var bksum = document.querySelector(".bksum").value
     let cardbox = document.createElement("div")
     cardbox.setAttribute("class", "card text-bg-secondary mb-3")
     cardbox.setAttribute("style", "max-width: 18rem;")
-    cardbox.innerHTML = "<div class='card-header'>Book Title</div> <div class='card-body'><h5 class='card-title'>Book Heading</h5><p class='card-text'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora nisi perspiciatis voluptatum repellendus. Odit, obcaecati laudantium velit voluptas at ad?</p><button type='button' onclick='delbtn(event)' class='btn btn-danger'> Danger </button></div></div >"
+    cardbox.innerHTML = "<div class='card-header'>"+bkhead+"</div> <div class='card-body'><h5 class='card-title'>"+bkauthor+"</h5><p class='card-text'>"+bksum+"</p><button type='button' onclick='delbtn(event)' class='btn btn-danger'> Danger </button></div></div >"
     bookbox.append(cardbox)
+
+    document.querySelector(".bkhead").value = "";
+    document.querySelector(".bkauth").value = "";
+    document.querySelector(".bksum").value = "";
+    addpage.style.display = "none";
 });
+
+
+
+    
+
 
 function delbtn(event){
     event.target.parentElement.parentElement.remove();
